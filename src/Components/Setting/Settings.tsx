d3a9fe768e9f7dd9221c96bd02b95337676624ff
Ahmed <aa87006722@gmail.com>
2025-06-24 21:15:14 +0300

    ( Refactor ) Convert Settings to Weather State Components and Restructure It To Be More Modern

--- src/Components/Setting/Settings.tsx
+++ src/Components/Setting/Settings.tsx
@@ -1,34 +1,12 @@
 
 export default function Settings() {
     return (
-        <div className="p-4 pl-8 w-56 h-56 absolute top-1/2 -translate-y-1/2 bg-gray-300/25 backdrop-blur-3xl rounded-4xl">
-            <h3 className="text-xl text-stone-300 my-2">Settings:</h3>
-            <form className="space-y-2 text-[14px]">
-                <div>
-                    <input type="radio" name="status" id="clear" />
-                    <label htmlFor="clear">Clear</label>
-                </div>
-
-                <div>
-                    <input type="radio" name="status" id="rain" />
-                    <label htmlFor="rain">Rain</label>
-                </div>
-
-                <div>
-                    <input type="radio" name="status" id="snow" />
-                    <label htmlFor="snow">Snow</label>
-                </div>
-
-                <div>
-                    <input type="radio" name="status" id="cloudy" />
-                    <label htmlFor="cloudy">Cloudy</label>
-                </div>
-
-                <div>
-                    <input type="radio" name="status" id="thunderstorm" />
-                    <label htmlFor="thunderstorm">Thunder Storm</label>
-                </div>
-            </form>
+        <div className="p-2 w-52 flex justify-between text-center absolute bottom-4 left-1/2 -translate-x-1/2 bg-gray-300/50 backdrop-blur-3xl rounded-4xl ">
+            <span className="cursor-pointer bg-white w-6 h-6 rounded-full font-bold hover:bg-gray-300 transition-all duration-150">&lt;</span>
+            <div className="ralative w-2/3 flex justify-center items-center">
+                <span className="item absolute">Rain</span>
+            </div>
+            <span className="cursor-pointer bg-white w-6 h-6 rounded-full font-bold hover:bg-gray-300 transition-all duration-150">&gt;</span>
         </div>
     )
 }
